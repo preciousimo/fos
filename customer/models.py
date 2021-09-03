9b95f3174f2d1d0e4f57f1ca64e7c11551525c55
preciousimo <preciousimoniakemu@gmail.com>
2021-09-04 00:11:15 +0100

    Shipping form logic

--- customer/models.py
+++ customer/models.py
@@ -35,6 +35,12 @@ class Order(models.Model):
         return str(self.id)
     
     @property
+    def shipping(self):
+        shipping = True
+        orderitems = self.orderitem_set.all()
+        return shipping
+    
+    @property
     def get_cart_total(self):
         orderitems = self.orderitem_set.all()
         total = sum([item.get_total for item in orderitems])
