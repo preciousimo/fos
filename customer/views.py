7d3275298c2451c62e1f01e8b932293e3f0c6c5a
preciousimo <preciousimoniakemu@gmail.com>
2021-09-03 21:17:15 +0100

    updateItem View

--- customer/views.py
+++ customer/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 from .models import*
 
@@ -32,3 +33,6 @@ def checkout(request):
 
     context = {'items':items, 'order':order}
     return render(request, 'customer/checkout.html', context)
+
+def updateItem(request):
+    return JsonResponse('Item was added', safe=False)
--- static/js/cart.js
+++ static/js/cart.js
@@ -14,3 +14,24 @@ for (i = 0; i < updateBtns.length; i++) {
     }
   })
 }
+
+function updateUserOrder(productId, action) {
+  console.log('User is authenticated, sending data...')
+
+  var url = '/update_item/'
+
+  fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRFToken': csrftoken,
+      },
+      body: JSON.stringify({ 'productId': productId, 'action': action })
+    })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log('data:', data)
+    });
+}
