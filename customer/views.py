69ed8174920c07ca45aacb45c62ecc9ba61072c6
preciousimo <preciousimoniakemu@gmail.com>
2021-09-03 14:53:10 +0100

    Setup static file

--- customer/views.py
+++ customer/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 
 def store(request):
-    	context = {}
-	return render(request, 'store/store.html', context)
+	context = {}
+	return render(request, 'customer/store.html', context)
 
 def cart(request):
 	context = {}
-	return render(request, 'store/cart.html', context)
+	return render(request, 'customer/cart.html', context)
 
 def checkout(request):
 	context = {}
-	return render(request, 'store/checkout.html', context)
+	return render(request, 'customer/checkout.html', context)
