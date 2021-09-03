c6363f3409997ff96b10b8bf5a6638ec7ac7cf3e
preciousimo <preciousimoniakemu@gmail.com>
2021-09-03 20:54:33 +0100

    Checkout page data

--- customer/views.py
+++ customer/views.py
@@ -21,5 +21,14 @@ def cart(request):
     return render(request, 'customer/cart.html', context)
 
 def checkout(request):
-	context = {}
-	return render(request, 'customer/checkout.html', context)
+    
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+
+    context = {'items':items, 'order':order}
+    return render(request, 'customer/checkout.html', context)
