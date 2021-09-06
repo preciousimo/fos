6fcf08a8ccb876f8700ec98cbf72d7a096d1a701
preciousimo <preciousimoniakemu@gmail.com>
2021-09-06 11:43:57 +0100

    Set cookie: add/remove item

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,6 +1,6 @@
 var updateBtns = document.getElementsByClassName('update-cart')
 
-for (i = 0; i < updateBtns.length; i++) {
+for (var i = 0; i < updateBtns.length; i++) {
   updateBtns[i].addEventListener('click', function() {
     var productId = this.dataset.product
     var action = this.dataset.action
@@ -13,6 +13,28 @@ for (i = 0; i < updateBtns.length; i++) {
       updateUserOrder(productId, action)
     }
   })
+}
+
+function addCookieItem(productId, action) {
+  console.log('Not logged in')
+
+  if (action == 'add') {
+    if (cart[productId] == undefined) {
+      cart[productId] = { 'quantity': 1 }
+    } else {
+      cart[productId]['quantity'] += 1
+    }
+  }
+  if (action == 'remove') {
+    cart[productId]['quantity'] -= 1
+
+    if (cart[productId]['quantity'] <= 0) {
+      console.log('Remove Item')
+      delete cart[productId]
+    }
+  }
+  console.log('Cart:', cart)
+  document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/"
 }
 
 function updateUserOrder(productId, action) {
