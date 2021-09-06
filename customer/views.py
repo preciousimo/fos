2f59348879688ff113b41e58e607ca4e3980f60e
preciousimo <preciousimoniakemu@gmail.com>
2021-09-06 14:44:59 +0100

    Update checkout

--- customer/views.py
+++ customer/views.py
@@ -4,7 +4,7 @@ import json
 import datetime
 
 from .models import*
-from .utils import cookieCart, cartData
+from .utils import cookieCart, cartData, guestOrder
 
 def store(request):
     
@@ -69,37 +69,9 @@ def processOrder(request):
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        
             
     else:
-        print ("You are not authenticated")
-        
-        print('COOKIES:', request.COOKIES)
-        name = data['form']['name']
-        email = data['form']['email']
-        
-        cookieData = cookieCart(request)
-        items = cookieData['items']
-        
-        customer, created = Customer.objects.get_or_create(
-            email=email,
-        )
-        customer.name = name
-        customer.save()
-        
-        order = Order.objects.create(
-            customer=customer,
-            complete=False,
-        )
-        
-        for item in items:
-            product = Product.objects.get(id=item['product']['id'])
-            
-            orderItem = OrderItem.objects.create(
-                product=product,
-                order=order,
-                quantity=item['quantity'],
-            )
+        customer, order = guestOrder(request, data)
             
     total = float(data['form']['total'])
     order.transaction_id = transaction_id
